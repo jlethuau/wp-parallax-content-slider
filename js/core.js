cc771385cc8fb1c023c61bf33d0a5db3235afc5a
jlethuau <julien.le.thuaut@gmail.com>
2013-05-17 14:44:10 +0200

    Fixed issue #7: https://github.com/jlethuau/wp-parallax-content-slider/issues/7

--- js/core.js
+++ js/core.js
@@ -1,5 +1,8 @@
 jQuery(function() {
-	jQuery('#da-slider').cslider({
+
+	// Fixed issue: https://github.com/jlethuau/wp-parallax-content-slider/issues/7
+	// Old code : only one slider per page
+/*	jQuery('#da-slider').cslider({
 		bgincrement : parseInt( prlx.bgincrement ),
 		autoplay    : parseInt( prlx.autoplay ),
 		interval    : parseInt( prlx.interval ),
@@ -10,4 +13,25 @@ jQuery(function() {
 		 swipeLeft:  function() { jQuery('#da-slider').find('span.da-arrows-next').click() },
 		 swipeRight: function() { jQuery('#da-slider').find('span.da-arrows-prev').click() },
 	})
+*/
+
+	jQuery('.da-slider').each( function( index ) {
+		
+		// Call cslider script on each parallax-slider
+		jQuery(this).cslider({
+								bgincrement : parseInt( prlx.bgincrement ),
+								autoplay    : parseInt( prlx.autoplay ),
+								interval    : parseInt( prlx.interval ),
+								current     : parseInt( prlx.current )
+							});
+	});
+
+	jQuery('.da-slider').each( function( index ) {
+		// Call touchSwipe script on each parallax-slider
+		jQuery(this).swipe({
+							 swipeLeft:  function() { jQuery('#da-slider').find('span.da-arrows-next').click() },
+							 swipeRight: function() { jQuery('#da-slider').find('span.da-arrows-prev').click() },
+							});
+	});
+
 });
